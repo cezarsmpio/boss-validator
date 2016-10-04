d475b1073b95d0f2f2279e1c14bfe2fe712b3ed7
haydenty <haydenty@users.noreply.github.com>
2016-10-04 18:55:07 -0500

    removed commas after please

--- src/js/boss.messages.js
+++ src/js/boss.messages.js
@@ -1,5 +1,5 @@
 const messages = {
-  default: 'Please, fill in this field.',
+  default: 'Please fill in this field.',
   required: 'This field is required.',
 
   // Numbers, Sizes
@@ -8,12 +8,12 @@ const messages = {
   bigger: 'The value needs to be bigger than {val}.',
   bigger_equal: 'The value needs to be less than or equal to {val}.',
   between: 'The value must be between {val}',
-  number: 'Please, enter a valid number.',
+  number: 'Please enter a valid number.',
 
 
   // Strings
   exact: 'Must contain the following characters: {val}',
-  extensions: 'Please, upload a file with the following extensions: {val}.',
+  extensions: 'Please upload a file with the following extensions: {val}.',
   contains: 'Must contain the following value: {val}.',
   minlength: 'Must be at least {val} characters long.',
   maxlength: 'Must be less than {val} characters long.',
@@ -24,12 +24,12 @@ const messages = {
   boolean: 'This field needs to be "true" or "false".',
 
   // Regex
-  email: 'Please, provide a valid email address. (ex: user@gmail.com)',
-  url: 'Please, provide a valid URL address with http:// or https://',
+  email: 'Please provide a valid email address. (ex: user@gmail.com)',
+  url: 'Please provide a valid URL address with http:// or https://',
   https: 'Your URL must start with https://',
-  credit_card: 'Please, enter a valid credit card number.',
-  ip_v4: 'Please, enter a valid IPV4 address. (ex: 172.16.254.1)',
-  ip_v6: 'Please, enter a valid IPV6 address. (ex: 3ffe:1900:4545:3:200:f8ff:fe21:67cf)',
+  credit_card: 'Please enter a valid credit card number.',
+  ip_v4: 'Please enter a valid IPV4 address. (ex: 172.16.254.1)',
+  ip_v6: 'Please enter a valid IPV6 address. (ex: 3ffe:1900:4545:3:200:f8ff:fe21:67cf)',
   alpha: 'Only letters are allowed.',
   alpha_numeric: 'No special characters allowed, just numbers and letters.',
 };
